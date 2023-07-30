954d75b3a11af0bf310ba15e14c4016410dfc674
zhangruinan <ruinan.zhang@icloud.com>
2023-07-30 01:21:46 -0700

    feat: initiate

---
+++ background.js
@@ -0,0 +1,12 @@
+chrome.browserAction.onClicked.addListener(function(tab) {
+    chrome.tabs.create({url: "http://www.mymap.ai/vizmap?type=vgmap&mid=pg-enFXnlaT66Zh&url=" + encodeURIComponent(tab.url)});
+  });
+  
+  chrome.commands.onCommand.addListener(function(command) {
+    if (command === "_execute_browser_action") {
+      chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        chrome.tabs.create({url: "http://www.mymap.ai/vizmap?type=vgmap&mid=pg-enFXnlaT66Zh&url=" + encodeURIComponent(tabs[0].url)});
+      });
+    }
+  });
+  
