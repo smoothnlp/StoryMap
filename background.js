1c86c4ca62cd4534552a323d691c669bc91420f2
luyunlei <luyunlei@58.com>
2023-07-30 17:58:18 +0800

    feat(master): 去掉mid参数
    Signed-off-by: luyunlei <luyunlei@58.com>

--- background.js
+++ background.js
@@ -1,11 +1,11 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
-    chrome.tabs.create({url: "http://www.mymap.ai/vizmap?type=vgmap&mid=pg-enFXnlaT66Zh&url=" + encodeURIComponent(tab.url)});
+    chrome.tabs.create({url: "http://www.mymap.ai/vizmap?type=vgmap&url=" + encodeURIComponent(tab.url)});
   });
   
   chrome.commands.onCommand.addListener(function(command) {
     if (command === "_execute_browser_action") {
       chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        chrome.tabs.create({url: "http://www.mymap.ai/vizmap?type=vgmap&mid=pg-enFXnlaT66Zh&url=" + encodeURIComponent(tabs[0].url)});
+        chrome.tabs.create({url: "http://www.mymap.ai/vizmap?type=vgmap&url=" + encodeURIComponent(tabs[0].url)});
       });
     }
   });
